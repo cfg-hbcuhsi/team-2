82551f17444845259e4668d93abc8490affb250c
hingende <59979524+hingende@users.noreply.github.com>
2020-09-12 05:43:38 -0400

    editing apis endpoints

--- financial_lit/home/views.py
+++ financial_lit/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def test(request):

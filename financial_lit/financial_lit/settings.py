1ab84de052a524e139d365d155ccf1c4c31e0c16
hingende <59979524+hingende@users.noreply.github.com>
2020-09-12 11:54:38 -0400

    cors enabled

--- financial_lit/financial_lit/settings.py
+++ financial_lit/financial_lit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
 
 ROOT_URLCONF = 'financial_lit.urls'
 

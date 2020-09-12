5b4cddc2390f27943db225beb647c69a806662a3
hingende <59979524+hingende@users.noreply.github.com>
2020-09-12 06:33:27 -0400

    apis

--- financial_lit/home/views.py
+++ financial_lit/home/views.py
@@ -5,13 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def test(request):
-
     return HttpResponse("hello")
 
 def login(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        pw = request.POST['password']
+        email = request.POST.get('email')
+        pw = request.POST.get('password')
         user = User.objects.filter(email = email)
         if bool(user) and user.password == pw:
             return HttpResponse(user.id)
@@ -19,13 +18,14 @@ def login(request):
 @csrf_exempt
 def sign_up(request):
     if request.method == 'POST':
-        fname = request.POST['First_Name']
-        lname = request.POST['Last_Name']
-        email = request.POST['email']
-        pw = request.POST['password']
-        b = User(first_name = fname, last_name = lname, email = email, password = pw)
+        fname = request.POST.get('First_Name')
+        lname = request.POST.get('Last_Name')
+        email = request.POST.get('email')
+        pw = request.POST.get('password')
+        b = User(first_name = "jalkjfda", last_name = lname, email = email, password = pw)
         b.save()
         return HttpResponse(b.id)
+    return HttpResponse("")
 
 def finance(request):
     return HttpResponse("")

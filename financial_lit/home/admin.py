796d271a4c3daae286e3b8edeb0725b450fd5baa
hingende <59979524+hingende@users.noreply.github.com>
2020-09-12 03:04:59 -0400

    data models registered

--- financial_lit/home/admin.py
+++ financial_lit/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import  *
 
+admin.site.register(Expenses)
+admin.site.register(User)
+admin.site.register(Goal)
+admin.site.register(Income)
 # Register your models here.
